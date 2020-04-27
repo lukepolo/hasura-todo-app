7ec1d301efce7d11a9c72c7f89d47b00a068e160
Luke Policinski <luke.policinski@egov.com>
2020-04-27 19:27:55 -0400

    simple example of taking in a JWT in and attaching to the request

--- routes/auth.js
+++ routes/auth.js
@@ -1,56 +1,19 @@
-import axios from "axios";
 import express from "express";
-import { firebase } from "./../auth/firebase";
-import setAuthSession from "./../auth/setAuthSession";
-
+import jsonwebtoken from "jsonwebtoken";
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  let { email, password } = req.body;
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .catch(function (error) {
-      res.status(error.code).send(error.message);
-    })
-    .then((response) => {
-      let user = response.user;
-      axios
-        .post("http://localhost:8080/v1/graphql", {
-          query: `
-						mutation {
-							insert_users(
-								objects : [
-									{
-										user_id : "${response.user.uid}"
-										name : "${response.user.displayName}"
-										email : "${response.user.email}"
-									}
-								]
-							) {
-								returning {
-									id
-								}
-							}
-						}
-					`,
-        })
-        .then(() => {
-          setAuthSession(user, res).then(() => {
-            res.end();
-          });
-        });
-    });
-});
+router.get("/hasura-webhook", (req, res) => {
+  let jwt = req.headers.authorization.replace("Bearer : ", "");
 
-router.post("/login", (req, res) => {
-  let { email, password } = req.body;
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch(function (error) {
-      res.status(error.code).send(error.message);
+  try {
+    let { user_id, role } = jsonwebtoken.verify(jwt, "my-secret");
+    return res.json({
+      "X-Hasura-Role": role,
+      "X-Hasura-User-Id": `${user_id}`,
     });
+  } catch (e) {
+    res.status(401).send("Not Authorized.");
+  }
 });
 
 export default router;
